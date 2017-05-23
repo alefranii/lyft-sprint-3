706661d2f8444eb6d5d7b3e464fcd9876ed246cf
alefranii <afmormar@gmail.com>
2017-05-23 17:00:00 -0400

    avances, falta js

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+//puntos extra:
+//formulario de validacion en el registro
+//todas las posiciones del tablero con alguna notificacion
+//los eventos deben ser creados con addEventListener( el usar onclick, directamente en html sera penalizado)
+
+//el nav es fijo y debe seguir al scroll del usuario
+//se debe crear una clase llamada auto que posea atributo "posicion_x" y un atributo "posicion-y" con 4 metodos:
+//* avanzar
+//*ir_izquierda
+/*ir_derecha
+*retroceder
+ en cada metodo debe sumar o restar 1 de posicion(x/y)
+dependiendo de la direccion
+*se debe crear un tablero para nuestro auto tiene que ser un arreglo de 10x6
+* en la seccion play with lyft debes agregar dos campos que soliciten al usuario las coordenadas (x,y) de la ubicacion inicial del auto.
+( se debe validar que dichas coordenadas no sean mayores al tamaño del trablero 10x6)
+
+al hacer clic en el boton star el formulario debe desaparecer, y en su lugar debe dibujarse un tablero con el auto en la posicion inicial ingresada por el usuario.
