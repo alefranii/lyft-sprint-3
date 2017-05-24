7c001fdb2f55e030f27b3e3cbb776011d9c60f3d
alefranii <afmormar@gmail.com>
2017-05-24 11:03:51 -0400

    iconos listos y css, falto vincular caja canvas y vincular boton envio formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,42 @@
-// falta todas las posiciones del tablero con alguna notificacion
-//los eventos deben ser creados con addEventListener( el usar onclick, directamente en html sera penalizado)
-//el nav es fijo y debe seguir al scroll del usuario
-//se debe crear una clase llamada auto que posea atributo "posicion_x" y un atributo "posicion-y" con 4 metodos:
-//* avanzar
-//*ir_izquierda
-//*ir_derecha
-//*retroceder
- //en cada metodo debe sumar o restar 1 de posicion(x/y)
-//dependiendo de la direccion
-//*se debe crear un tablero para nuestro auto tiene que ser un arreglo de 10x6
-//* en la seccion play with lyft debes agregar dos campos que soliciten al usuario las coordenadas (x,y) de la ubicacion inicial del auto.
-//( se debe validar que dichas coordenadas no sean mayores al tamaño del trablero 10x6)
+ //validacion de boton hazte conductor
 
-//al hacer clic en el boton star el formulario debe desaparecer, y en su lugar debe dibujarse un tablero con el auto en la posicion inicial ingresada por el usuario.
+ function validateForm() {
+    document.getElementById("driverBtn").addEventListener("click", function(){
+     console.log('input-number: ' + document.getElementById('input-number').value)
+   console.log('input-number: ' + document.getElementById('input-number').value);
+      if (document.getElementById('input-number').value != "") {
+     alert('le enviamos un mensaje a su celular con las intrucciones')
+   } else {
+     alert('inserte numero valido')
+   }
+ });
+       alert('le enviamos un mensaje a su celular con las instrucciones');
+     } else {
+       alert('Inserte numero valido');
+     }
+   };
+   event.preventDefault();
+  }
+
+
+
+//validacion boton "vamonos" el formulario debe desaparecer, y en su lugar debe dibujarse un tablero con el auto en la posicion inicial ingresada por el usuario.
+var caja1 = document.getElementById('vamonos'),
+    caja2 = document.getElementById('volver'),
+    botonVolver = caja2.querySelector('button');
+
+function vamos(e) {
+  caja1.classList.add('hidden');
+  caja2.classList.remove('hidden');
+}
+
+botonVolver.addEventListener(
+  'click',
+  function volvamos(e) {
+    caja2.classList.add('hidden');
+    caja1.classList.remove('hidden');
+  }
+)
 
 
 //funciones para caja del auto
